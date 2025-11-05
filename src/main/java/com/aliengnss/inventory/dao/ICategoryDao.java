0d32fae96cbe008c7541a273257acfc677305613
Christian <chris@dev.com>
2025-11-04 21:17:38 -0600

    Cambio de crud repository a jpa repository

--- src/main/java/com/aliengnss/inventory/dao/ICategoryDao.java
+++ src/main/java/com/aliengnss/inventory/dao/ICategoryDao.java
@@ -1,7 +1,7 @@
 package com.aliengnss.inventory.dao;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.aliengnss.inventory.model.Category;
 
-public interface ICategoryDao extends CrudRepository<Category, Long> {
+public interface ICategoryDao extends JpaRepository<Category, Long> {
     
 }
